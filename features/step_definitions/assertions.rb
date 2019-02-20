dee15e4c079cf26bfcb9ebf40abc083909cb4b23
Thomas <thomas@craftacademy.se>
2019-02-20 22:51:48 +0100

    fixes step definition

--- features/step_definitions/assertions.rb
+++ features/step_definitions/assertions.rb
@@ -1,16 +1,20 @@
-Then("I should see a list of articles") do
+# frozen_string_literal: true
+
+Then('I should see a list of articles') do
   expect(page).to have_selector '#article-list'
 end
 
-Then("I should see {string}") do |expected_content|
-  within("#article_#{@article.id}") do
+Then('I should see {string}') do |expected_content|
+  if @article
+    within("#article_#{@article.id}") do
+      expect(page).to have_text expected_content
+    end
+  else
     expect(page).to have_text expected_content
   end
 end
 
-Then("I should not see {string}") do |expected_content|
-
-
+Then('I should not see {string}') do |expected_content|
   within("#article_#{@article.id}") do
     expect(page).not_to have_text expected_content
   end
